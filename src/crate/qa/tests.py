04565532a76ab67ad8e901a829e809f8083de082
Michael Beer <michael@crate.io>
2017-09-26 10:33:01 +0200

    added test case for blob storage

--- src/crate/qa/tests.py
+++ src/crate/qa/tests.py
@@ -2,7 +2,7 @@
 
 import shutil
 import tempfile
-import unittest
+import time
 
 from cr8.run_crate import CrateNode, get_crate
 
@@ -38,3 +38,17 @@ class NodeProvider:
         for to_stop in self._on_stop:
             to_stop()
         self._on_stop.clear()
+
+    def _wait_for_active_shards(self, cursor):
+        """Wait until all shards are started"""
+        waited = 0
+        duration = 0.01
+        while waited < 20:
+            cursor.execute(
+                "SELECT count(*) FROM sys.shards WHERE state != 'STARTED'")
+            if int(cursor.fetchone()[0]) == 0:
+                return
+            time.sleep(duration)
+            waited += duration
+            duration *= 2
+        raise TimeoutError("Shards didn't become active in time")
---
+++ tests/restart/BlobTestCase.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import unittest
+from io import BytesIO
+from pathlib import Path
+from crate.client import connect
+from crate.qa.tests import NodeProvider
+
+
+class BlobTestCase(NodeProvider, unittest.TestCase):
+
+    CRATE_VERSION = 'latest-nightly'
+
+    def test_blob_index(self):
+        node = self._new_node(self.CRATE_VERSION)
+        node.start()
+        with connect(node.http_url) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+            CREATE BLOB TABLE myblobs
+            CLUSTERED INTO 1 shards
+            WITH (number_of_replicas = 0)
+            """)
+        node.stop()
+
+        node.start()
+        with connect(node.http_url) as conn:
+            cursor = conn.cursor()
+            self._wait_for_active_shards(cursor)
+            cursor.execute("""
+            SELECT table_name, number_of_shards, number_of_replicas
+            FROM information_schema.tables
+            WHERE table_schema = 'blob'
+            """)
+            result = cursor.fetchone()
+            self.assertEqual(result[0], 'myblobs')
+            self.assertEqual(result[1], 1)
+            self.assertEqual(result[2], '0')
+
+    def test_blob_record(self):
+        node = self._new_node(self.CRATE_VERSION)
+        node.start()
+        digest = ''
+        with connect(node.http_url) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+            CREATE BLOB TABLE myblobs
+            CLUSTERED INTO 1 shards
+            WITH (number_of_replicas = 0)
+            """)
+            blob_container = conn.get_blob_container('myblobs')
+            digest = blob_container.put(BytesIO(b'sample data'))
+        node.stop()
+
+        node.start()
+        with connect(node.http_url) as conn:
+            cursor = conn.cursor()
+            self._wait_for_active_shards(cursor)
+            cursor.execute("SELECT count(*) FROM blob.myblobs WHERE digest = ?", (digest,))
+            result = cursor.fetchone()
+            self.assertEqual(result[0], 1)
+
+            blob_container = conn.get_blob_container('myblobs')
+            result = blob_container.get(digest)
+            self.assertTrue(blob_container.exists(digest))
+            self.assertEqual(next(result), b'sample data')
+            filepath = Path(self._path_data).glob(f'**/{digest}')
+            self.assertTrue(next(filepath).exists())
+
+
+def test_suite():
+    """
+    To be executed with `python -m unittest` from same directory as this file.
+    """
+    return unittest.makeSuite(BlobTestCase)
+
+
+if __name__ == '__main__':
+    """
+    To be executed from anywhere using `python path/to/tests.py`.
+    """
+    unittest.main(verbosity=2)
