49a0d725a4a4d3bf056058b67c67ab093237b22d
Andrei Dan <andrei@crate.io>
2017-11-23 11:14:57 +0000

    Catch execution exceptions and fail the future so we can gracefully shut down.
    In case of unhandled exceptions the test would print a meaningless exception
    when trying to close the connection.
    This commit wraps our test code into a try-catch block and fails the execution
    future when an unexpected exception occurs. This will also provide a meaningful
    failure message and stacktrace.

--- tests/client_tests/python/asyncpg/test_asyncpg.py
+++ tests/client_tests/python/asyncpg/test_asyncpg.py
@@ -15,56 +15,61 @@ class AsyncpgTestCase(NodeProvider, unittest.TestCase):
         async def run(hosts, future):
             pool = await asyncpg.create_pool(f'postgres://crate@{hosts}/doc')
             async with pool.acquire() as conn:
-                create_result = await conn.execute("CREATE TABLE t1 (x int \
-                        primary key, y int)")
-                if int(create_result[7]) != 1:
-                    future.set_exception(AssertionError("CREATE statement \
+                try:
+                    create_result = await conn.execute("CREATE TABLE t1 (x int \
+                            primary key, y int)")
+                    if int(create_result[7]) != 1:
+                        future.set_exception(AssertionError("CREATE statement \
 should've returned count 1 but the result was: " + create_result))
 
-                insert_result = await conn.execute('INSERT INTO t1(x) VALUES(?)', 1)
-                if int(insert_result[9]) != 1:
-                    future.set_exception(AssertionError("INSERT statement \
+                    insert_result = await conn.execute('INSERT INTO t1(x) VALUES(?)', 1)
+                    if int(insert_result[9]) != 1:
+                        future.set_exception(AssertionError("INSERT statement \
 should've returned count 1 but the result was: " + insert_result))
 
-                refresh_result = await conn.execute('REFRESH TABLE t1')
-                if int(refresh_result[8]) != 1:
-                    future.set_exception(AssertionError("REFRESH statement \
+                    refresh_result = await conn.execute('REFRESH TABLE t1')
+                    if int(refresh_result[8]) != 1:
+                        future.set_exception(AssertionError("REFRESH statement \
 should've returned count 1 but the result was: " + refresh_result))
 
-                update_result = await conn.execute('UPDATE t1 SET y = ?', 2)
-                if int(update_result[7]) != 1:
-                    future.set_exception(AssertionError("UPDATE statement \
+                    update_result = await conn.execute('UPDATE t1 SET y = ?', 2)
+                    if int(update_result[7]) != 1:
+                        future.set_exception(AssertionError("UPDATE statement \
 should've returned count 1 but the result was: " + update_result))
 
-                refresh_result = await conn.execute('REFRESH TABLE t1')
-                if int(refresh_result[8]) != 1:
-                    future.set_exception(AssertionError("REFRESH statement \
+                    refresh_result = await conn.execute('REFRESH TABLE t1')
+                    if int(refresh_result[8]) != 1:
+                        future.set_exception(AssertionError("REFRESH statement \
 should've returned count 1 but the result was: " + refresh_result))
 
-                delete_result = await conn.execute('DELETE FROM t1 WHERE y = ?', 2)
-                if int(delete_result[7]) != 1:
-                    future.set_exception(AssertionError("DELETE statement \
+                    delete_result = await conn.execute('DELETE FROM t1 WHERE y = ?', 2)
+                    if int(delete_result[7]) != 1:
+                        future.set_exception(AssertionError("DELETE statement \
 should've returned count 1 but the result was: " + delete_result))
 
-                insert_from_subquery_result = await conn.execute('INSERT INTO \
-                        t1 (x) (SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?)', 1)
-                if int(insert_from_subquery_result[9]) != 1:
-                    future.set_exception(AssertionError("INSERT FROM SUBQUERY \
+                    insert_from_subquery_result = await conn.execute('INSERT INTO \
+                            t1 (x) (SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?)', 1)
+                    if int(insert_from_subquery_result[9]) != 1:
+                        future.set_exception(AssertionError("INSERT FROM SUBQUERY \
 should've returned count 1 but the result was: " + insert_from_subquery_result))
 
-                insert_subquery_on_duplicate = await conn.execute('INSERT INTO \
-                        t1 (x) (SELECT col1 FROM unnest([1, 2]) \
-                        WHERE col1 = ?) ON DUPLICATE KEY UPDATE x = ?', 1, 2)
-                if int(insert_subquery_on_duplicate[9]) != 1:
-                    future.set_exception(AssertionError("INSERT FROM SUBQUERY \
+                    insert_subquery_on_duplicate = await conn.execute('INSERT INTO \
+                            t1 (x) (SELECT col1 FROM unnest([1, 2]) \
+                            WHERE col1 = ?) ON DUPLICATE KEY UPDATE x = ?', 1, 2)
+                    if int(insert_subquery_on_duplicate[9]) != 1:
+                        future.set_exception(AssertionError("INSERT FROM SUBQUERY \
 with ON DUPLICATE KEY clause should've returned count 1 but the result was:\
 " + insert_subquery_on_duplicate))
 
-                if not future.done():
-                    future.set_result("Success!")
+                    if not future.done():
+                        future.set_result("Success!")
 
-                # Close the connection.
-                await conn.close()
+                except Exception as e:
+                    future.set_exception(e)
+
+                finally:
+                    # Close the connection.
+                    await conn.close()
 
         loop = asyncio.get_event_loop()
         future = asyncio.Future()
