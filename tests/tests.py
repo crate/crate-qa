87798ecdc3c371ece6fbd217f6eb1db8693e33f0
Mathias Fussenegger <f.mathias@zignar.net>
2018-03-13 15:11:47 +0100

    Use mypy for type checks

--- tests/tests.py
+++ tests/tests.py
@@ -7,9 +7,9 @@ def suite():
     """
     return unittest.TestLoader().discover('.', pattern='test_*.py')
 
+
 if __name__ == '__main__':
     """
     To be executed from anywhere using `python path/to/tests.py`.
     """
-    suite = suite()
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite())
