abfac8f24558ae44e6e091a0042e141f2e512680
Andrei Dan <andrei@crate.io>
2018-06-29 16:47:29 +0100

    Adjust the pg_type workaround to avoid errors when using pgx.

--- tests/client_tests/go/setup.sql
+++ tests/client_tests/go/setup.sql
@@ -6,11 +6,25 @@ create table doc.pg_type (
     typdelim string,
     typelem int,
     typname string,
-    typtype string
+    typtype string,
+    typnamespace int,
+    typbasetype int
 );
 
-insert into doc.pg_type (oid, typdelim, typelem, typname, typtype)
-    (select oid, typdelim, typelem, typname, typtype from pg_catalog.pg_type);
+insert into doc.pg_type (oid, typdelim, typelem, typname, typtype, typnamespace, typbasetype)
+    (select oid, typdelim, typelem, typname, typtype, 11, 0 from pg_catalog.pg_type);
+refresh table doc.pg_type;
+
+update doc.pg_type set typname='timestamptz' where typname='timestampz';
+update doc.pg_type set typname='_timestamptz' where typname='_timestampz';
+
+create table doc.pg_namespace (
+    oid int,
+    nspname string
+);
+
+insert into doc.pg_namespace (oid, nspname) values (11, 'pg_catalog');
+refresh table doc.pg_namespace;
 
 CREATE TABLE doc.users (
   id LONG,
