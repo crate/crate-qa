d0315e0160d0afcf01036db1ce413b18ff5be774
Mathias Fussenegger <f.mathias@zignar.net>
2018-07-10 15:02:38 +0200

    Extend basic go tests to verify inserted timestamp
    Depends on https://github.com/crate/crate/pull/7521

--- tests/client_tests/go/basic_queries.go
+++ tests/client_tests/go/basic_queries.go
@@ -21,49 +21,50 @@ func main() {
 	conn, err := pgx.Connect(pgxConfig)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	var name string
 	err = conn.QueryRow("select name || $1 from sys.cluster", "foo").Scan(&name)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(name)
 	commandTag, err := conn.Exec("create table t1 (x integer, ts timestamp)")
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
-	commandTag, err = conn.Exec("insert into t1 (x, ts) values (?, ?)", 1, time.Now())
+	ts := time.Now()
+	commandTag, err = conn.Exec("insert into t1 (x, ts) values (?, ?)", 1, ts)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
 	commandTag, err = conn.Exec("refresh table t1")
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
+	var tsRead time.Time
+	err = conn.QueryRow("select ts from t1").Scan(&tsRead)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if (tsRead.Sub(ts) > (1 * time.Second)) {
+		log.Fatal("Inserted ts doesn't match read ts: ", ts, tsRead)
+	}
 	commandTag, err = conn.Exec("update t1 set x = ?", 2)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
 	commandTag, err = conn.Exec("refresh table t1")
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
 	commandTag, err = conn.Exec("delete from t1 where x = ?", 2)
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	fmt.Println(commandTag)
 }
