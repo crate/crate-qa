422b9249ee4464b3669ec5d06902f6c42cb43638
Mathias Fussenegger <f.mathias@zignar.net>
2019-07-05 11:51:20 +0200

    Remove pg_type hacks from pgx client tests
    They're no longer required with all the compatibility improvements we've
    made.

--- tests/client_tests/go/setup.sql
+++ tests/client_tests/go/setup.sql
@@ -1,31 +1,5 @@
 -- dbext:type=CRATE:host=localhost:port=4200:dbname=doc
 
-
-create table doc.pg_type (
-    oid int,
-    typdelim string,
-    typelem int,
-    typname string,
-    typtype string,
-    typnamespace int,
-    typbasetype int
-);
-
-insert into doc.pg_type (oid, typdelim, typelem, typname, typtype, typnamespace, typbasetype)
-    (select oid, typdelim, typelem, typname, typtype, 11, 0 from pg_catalog.pg_type);
-refresh table doc.pg_type;
-
-update doc.pg_type set typname='timestamptz' where typname='timestampz';
-update doc.pg_type set typname='_timestamptz' where typname='_timestampz';
-
-create table doc.pg_namespace (
-    oid int,
-    nspname string
-);
-
-insert into doc.pg_namespace (oid, nspname) values (11, 'pg_catalog');
-refresh table doc.pg_namespace;
-
 CREATE TABLE doc.users (
   id LONG,
   name STRING,
