00da6087c60c700b31d58def18ad5f4c48546670
Sebastian Utz <su@rtme.net>
2020-11-05 09:26:52 +0100

    WIP: Debug go docker tests, print go version

--- tests/client_tests/go/run.sh
+++ tests/client_tests/go/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-go get -u "github.com/jackc/pgx"
+go version
+go env
+go get -u -v "github.com/jackc/pgx"
 cr8 run-crate latest-nightly \
     -- @crash --hosts '{node.http_url}' < $(dirname "$0")/setup.sql \
     -- @go run $(dirname "$0")/basic_queries.go \
