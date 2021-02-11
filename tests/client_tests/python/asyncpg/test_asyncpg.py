b3c6ff0780bd8c6ade28d9109dbe302897ab70f9
Andreas Motl <andreas.motl@crate.io>
2021-02-11 18:38:28 +0100

    asyncpg: Slightly refactor test layout
    This patch is just a slight refactoring in order to bring in more test
    cases afterwards. It does not bring in any semantic changes.

--- tests/client_tests/python/asyncpg/test_asyncpg.py
+++ tests/client_tests/python/asyncpg/test_asyncpg.py
@@ -4,45 +4,54 @@ import unittest
 from crate.qa.tests import NodeProvider
 
 
-async def exec_queries(test, hosts):
-    pool = await asyncpg.create_pool(f'postgres://crate@{hosts}/doc')
-    async with pool.acquire() as conn:
-        result = await conn.execute(
-            "CREATE TABLE t1 (x int primary key, y int)")
-        test.assertEqual(result, 'CREATE 1')
+async def basic_queries(test, conn):
+    """
+    Invoke some basic queries.
+    """
 
-        result = await conn.execute('INSERT INTO t1 (x) VALUES (?)', 1)
-        test.assertEqual(result, 'INSERT 0 1')
+    await conn.execute("DROP TABLE IF EXISTS t1")
 
-        result = await conn.execute('REFRESH TABLE t1')
-        test.assertEqual(result, 'REFRESH 1')
+    result = await conn.execute(
+        "CREATE TABLE t1 (x int primary key, y int)")
+    test.assertEqual(result, 'CREATE 1')
 
-        result = await conn.execute('UPDATE t1 SET y = ?', 2)
-        test.assertEqual(result, 'UPDATE 1')
+    result = await conn.execute('INSERT INTO t1 (x) VALUES (?)', 1)
+    test.assertEqual(result, 'INSERT 0 1')
 
-        result = await conn.execute('REFRESH TABLE t1')
-        test.assertEqual(result, 'REFRESH 1')
+    result = await conn.execute('REFRESH TABLE t1')
+    test.assertEqual(result, 'REFRESH 1')
 
-        result = await conn.execute('DELETE FROM t1 WHERE y = ?', 2)
-        test.assertEqual(result, 'DELETE 1')
+    result = await conn.execute('UPDATE t1 SET y = ?', 2)
+    test.assertEqual(result, 'UPDATE 1')
 
-        result = await conn.execute('''
-            INSERT INTO t1 (x) (
-                SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?
-            )
-        ''', 1)
-        test.assertEqual(result, 'INSERT 0 1')
+    result = await conn.execute('REFRESH TABLE t1')
+    test.assertEqual(result, 'REFRESH 1')
 
-        result = await conn.execute('''
-            INSERT INTO t1 (x) (
-                SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?)
-            ON CONFLICT (x) DO UPDATE SET y = ?
-        ''', 1, 2)
-        test.assertEqual(result, 'INSERT 0 1')
+    result = await conn.execute('DELETE FROM t1 WHERE y = ?', 2)
+    test.assertEqual(result, 'DELETE 1')
 
-        result = await conn.fetch('SELECT pg_catalog.pg_get_keywords()')
-        keyword = sorted([row[0] for row in result], key=lambda x: x[0])[0]
-        test.assertEqual(keyword, ('add', 'R', 'reserved'))
+    result = await conn.execute('''
+        INSERT INTO t1 (x) (
+            SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?
+        )
+    ''', 1)
+    test.assertEqual(result, 'INSERT 0 1')
+
+    result = await conn.execute('''
+        INSERT INTO t1 (x) (
+            SELECT col1 FROM unnest([1, 2]) WHERE col1 = ?)
+        ON CONFLICT (x) DO UPDATE SET y = ?
+    ''', 1, 2)
+    test.assertEqual(result, 'INSERT 0 1')
+
+
+async def binary_streaming(test, conn):
+    """
+    Verify that binary streaming of record types works (2020-02-07).
+    """
+    result = await conn.fetch('SELECT pg_catalog.pg_get_keywords()')
+    keyword = sorted([row[0] for row in result], key=lambda x: x[0])[0]
+    test.assertEqual(keyword, ('add', 'R', 'reserved'))
 
 
 async def fetch_summits(test, host, port):
@@ -59,6 +68,13 @@ async def fetch_summits(test, host, port):
     test.assertEqual(fourth['mountain'], 'Liskamm')
 
 
+async def exec_queries_pooled(test, hosts):
+    pool = await asyncpg.create_pool(f'postgres://crate@{hosts}/doc')
+    async with pool.acquire() as conn:
+        await basic_queries(test, conn)
+        await binary_streaming(test, conn)
+
+
 class AsyncpgTestCase(NodeProvider, unittest.TestCase):
 
     def test_basic_statements(self):
@@ -67,7 +83,7 @@ class AsyncpgTestCase(NodeProvider, unittest.TestCase):
         loop = asyncio.get_event_loop()
         psql_addr = node.addresses.psql
         crate_psql_url = f'{psql_addr.host}:{psql_addr.port}'
-        loop.run_until_complete(exec_queries(self, crate_psql_url))
+        loop.run_until_complete(exec_queries_pooled(self, crate_psql_url))
 
     def test_result_streaming_using_fetch_size(self):
         (node, _) = self._new_node(self.CRATE_VERSION)
