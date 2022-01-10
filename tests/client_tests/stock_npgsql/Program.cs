54617e10f807ce4cbecc5c0e2a836480ce12a1b7
Mathias Fussenegger <f.mathias@zignar.net>
2022-01-10 11:08:16 +0100

    Add entity framework insert test for npgsql
    Requires https://github.com/crate/crate/pull/12044

--- tests/client_tests/stock_npgsql/Program.cs
+++ tests/client_tests/stock_npgsql/Program.cs
@@ -4,12 +4,49 @@ using System.Diagnostics;
 using Npgsql;
 using System.Threading.Tasks;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace stock_npgsql
 {
+    public class TestEntity
+    {
+        public String Id { get; set; }
+        public DateTimeOffset DateTime{ get; set; } = DateTimeOffset.UtcNow;
+
+        public TestEntity() {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+
+    public class CrateContext : DbContext
+    {
+          public virtual DbSet<TestEntity> Test { get; set; }
+          public static string ConnectionString { get; set; }
+
+          public CrateContext() { }
+
+          public CrateContext(DbContextOptions<CrateContext> options) : base(options) { }
+
+          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          {
+              optionsBuilder.UseNpgsql(ConnectionString);
+          }
+
+          protected override void OnModelCreating(ModelBuilder modelBuilder)
+          {
+              modelBuilder.Entity<TestEntity>(entity =>
+              {
+                  entity.HasKey(e => e.Id);
+                  entity.ToTable("test", "test");
+                  entity.Property(e => e.Id).HasColumnName("id").HasColumnType("text");
+                  entity.Property(e => e.DateTime).HasColumnName("datetime").HasColumnType("timestamp with time zone");
+              });
+          }
+    }
+
     class Program
     {
-
         public static async Task TestUnnestAsync(NpgsqlConnection conn)
         {
             using var command = new NpgsqlCommand(
@@ -39,54 +76,85 @@ namespace stock_npgsql
             var connString = $"Host={host};Port={port};Username=crate;Password=;Database=doc";
             using (var conn = new NpgsqlConnection(connString)) {
                 conn.Open();
-                using (var cmd = new NpgsqlCommand("SELECT mountain FROM sys.summits ORDER BY 1 LIMIT 10", conn))
-                using (var reader = cmd.ExecuteReader()) {
-                    List<string> mountains = new List<string>();
-                    while (reader.Read()) {
-                        mountains.Add(reader.GetString(0));
-                    }
-                    Debug.Assert(mountains[0] == "Acherkogel");
-                }
 
-                using (var cmd = new NpgsqlCommand("CREATE TABLE tbl (x int)", conn)) {
-                    cmd.ExecuteNonQuery();
-                }
-                using (var cmd = new NpgsqlCommand("INSERT INTO tbl (x) VALUES (@x)", conn))
-                {
-                    cmd.Parameters.AddWithValue("x", 10);
-                    cmd.ExecuteNonQuery();
-                }
-                using (var cmd = new NpgsqlCommand("INSERT INTO tbl (x) VALUES (@x)", conn))
-                {
-                    using (var transaction = conn.BeginTransaction())
-                    {
-                        cmd.Transaction = transaction;
-                        cmd.Parameters.Add("@x", NpgsqlTypes.NpgsqlDbType.Integer);
-
-                        for (int i = 1; i < 10; i++)
-                        {
-                            cmd.Parameters["@x"].Value = i * 10;
-                            cmd.ExecuteNonQuery();
-                        }
-
-                        transaction.Commit();
-                    }
-                }
-                using (var cmd = new NpgsqlCommand("REFRESH TABLE tbl", conn))
-                {
-                    cmd.ExecuteNonQuery();
-                }
-
-                using (var cmd = new NpgsqlCommand("SELECT x FROM tbl ORDER BY 1 ASC LIMIT 10", conn))
-                using (var reader = cmd.ExecuteReader())
-                {
-                    Debug.Assert(reader.Read());
-                    int value = (int) reader[0];
-                    Debug.Assert(value == 10, "first value must be 10, but is " + value);
-                }
-
+                await TestBasics(conn);
                 await TestUnnestAsync(conn);
+                await TestInsertUsingEntityFramework(conn);
             }
+        }
+
+        private static async Task TestBasics(NpgsqlConnection conn)
+        {
+            using (var cmd = new NpgsqlCommand("SELECT mountain FROM sys.summits ORDER BY 1 LIMIT 10", conn))
+            using (var reader = cmd.ExecuteReader()) {
+                List<string> mountains = new List<string>();
+                while (reader.Read()) {
+                    mountains.Add(reader.GetString(0));
+                }
+                Debug.Assert(mountains[0] == "Acherkogel");
+            }
+
+            using (var cmd = new NpgsqlCommand("CREATE TABLE tbl (x int)", conn)) {
+                cmd.ExecuteNonQuery();
+            }
+            using (var cmd = new NpgsqlCommand("INSERT INTO tbl (x) VALUES (@x)", conn))
+            {
+                cmd.Parameters.AddWithValue("x", 10);
+                cmd.ExecuteNonQuery();
+            }
+            using (var cmd = new NpgsqlCommand("INSERT INTO tbl (x) VALUES (@x)", conn))
+            {
+                using (var transaction = conn.BeginTransaction())
+                {
+                    cmd.Transaction = transaction;
+                    cmd.Parameters.Add("@x", NpgsqlTypes.NpgsqlDbType.Integer);
+
+                    for (int i = 1; i < 10; i++)
+                    {
+                        cmd.Parameters["@x"].Value = i * 10;
+                        await cmd.ExecuteNonQueryAsync();
+                    }
+
+                    await transaction.CommitAsync();
+                }
+            }
+            using (var cmd = new NpgsqlCommand("REFRESH TABLE tbl", conn))
+            {
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+            using (var cmd = new NpgsqlCommand("SELECT x FROM tbl ORDER BY 1 ASC LIMIT 10", conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                Debug.Assert(reader.Read());
+                int value = (int) reader[0];
+                Debug.Assert(value == 10, "first value must be 10, but is " + value);
+            }
+        }
+
+        private static async Task TestInsertUsingEntityFramework(NpgsqlConnection conn)
+        {
+            using (var cmd = new NpgsqlCommand("drop table if exists test.test", conn))
+            {
+                await cmd.ExecuteNonQueryAsync();
+            }
+            string createTable = @"
+                CREATE TABLE test.test (
+                    id text default gen_random_text_uuid() primary key,
+                    datetime timestamp with time zone
+                )";
+            using (var cmd = new NpgsqlCommand(createTable, conn))
+            {
+                await cmd.ExecuteNonQueryAsync();
+            }
+            CrateContext.ConnectionString = conn.ConnectionString;
+            using CrateContext context = new();
+
+            for (int i = 0; i < 4; i++)
+            {
+                context.Test.Add(new TestEntity());
+            }
+            await context.SaveChangesAsync();
         }
     }
 }
