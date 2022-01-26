e5a3c1c066bbf4838f839b58c212c13800b008c3
Mathias Fussenegger <f.mathias@zignar.net>
2022-01-26 09:22:05 +0100

    Add 4.7.x to bwc tests

--- tests/bwc/test_upgrade.py
+++ tests/bwc/test_upgrade.py
@@ -30,6 +30,7 @@ UPGRADE_PATHS = (
         VersionDef('4.4.x', False, []),
         VersionDef('4.5.x', False, []),
         VersionDef('4.6.x', False, []),
+        VersionDef('4.7.x', False, []),
         VersionDef('latest-nightly', False, [])
     ),
 )
