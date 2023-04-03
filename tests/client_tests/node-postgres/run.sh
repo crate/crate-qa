0f1dfa09f6286a11709708aa691a78dd3429f063
Mathias Fussenegger <f.mathias@zignar.net>
2023-04-03 14:06:27 +0200

    Use jest to run node-postgres tests
    Replaces the custom test runner to have something that may be more
    familiar to people.

--- tests/client_tests/node-postgres/run.sh
+++ tests/client_tests/node-postgres/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cr8 run-crate latest-nightly \
-  -- @node main.js {node.addresses.psql.host} {node.addresses.psql.port}
+cr8 run-crate latest-nightly -s psql.port=5432 \
+  -- @npm test
