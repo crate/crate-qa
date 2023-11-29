a764754ded66560d6762f2c68a081adeb297f490
Andreas Motl <andreas.motl@crate.io>
2023-11-29 20:18:09 +0100

    Update Npgsql test to use .NET 8
    It is needed for using Npgsql 8, because .NET 6 is no longer supported.

--- tests/client_tests/stock_npgsql/run.sh
+++ tests/client_tests/stock_npgsql/run.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 
 dotnet build
 cr8 run-crate latest-nightly \
-  -- @$(pwd)/bin/Debug/net6.0/stock_npgsql {node.addresses.psql.host} {node.addresses.psql.port}
+  -- @$(pwd)/bin/Debug/net8.0/stock_npgsql {node.addresses.psql.host} {node.addresses.psql.port}
