4d4ac4585bd5269cff730c58769121408ae61c8e
baur <baurzhansahariev@gmail.com>
2024-07-17 16:47:57 +0500

    Add CrateDB 5.8.0 to BWC tests

--- tests/bwc/test_recovery.py
+++ tests/bwc/test_recovery.py
@@ -22,7 +22,8 @@ UPGRADE_PATHS = [
     UpgradePath('5.4.x', '5.5.x'),
     UpgradePath('5.5.x', '5.6.x'),
     UpgradePath('5.6.x', '5.7.x'),
-    UpgradePath('5.7.x', 'latest-nightly')
+    UpgradePath('5.7.x', '5.8.x'),
+    UpgradePath('5.8.x', 'latest-nightly')
 ]
 UPGRADE_PATHS_FROM_43 = [UpgradePath('4.3.x', '4.4.x')]
 
