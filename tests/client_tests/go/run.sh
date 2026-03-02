bd206d730f2916f7dd38b2e4bfeb6b4918a983fb
Mathias Fussenegger <f.mathias@zignar.net>
2026-03-02 09:11:15 +0100

    Add `Ping` call to pgx tests
    Requires https://github.com/crate/crate/pull/19095

--- tests/client_tests/go/run.sh
+++ tests/client_tests/go/run.sh
@@ -3,12 +3,14 @@
 go mod init main
 go mod tidy
 
+dir=$(dirname "$0")
+
 cr8 run-crate latest-nightly \
-    -- @crash --hosts '{node.http_url}' < $(dirname "$0")/setup.sql \
-    -- @go run $(dirname "$0")/basic_queries.go \
+    -- @crash --hosts '{node.http_url}' < "$dir/setup.sql" \
+    -- @go run "$dir/basic_queries.go" \
         --hosts '{node.addresses.psql.host}' \
         --port '{node.addresses.psql.port}' \
-    -- @go run $(dirname "$0")/bulk_operations.go \
+    -- @go run "$dir/bulk_operations.go" \
         --hosts '{node.addresses.psql.host}' \
         --port '{node.addresses.psql.port}' \
     -- @echo "SUCCESS"
