0705cf5053403bf38a48c2abb8535c4cc1403461
Mathias Fussenegger <f.mathias@zignar.net>
2026-09-14 09:53:37 +0200

    Replace minio with silo
    Minio was abandoned and the download failed with a 410 Gone error.
    silo is a community fork https://github.com/pgsty/silo

--- src/crate/qa/minio_svr.py
+++ src/crate/qa/minio_svr.py
@@ -5,6 +5,7 @@ import platform
 import shutil
 import tempfile
 import socket
+import tarfile
 from pathlib import Path
 from subprocess import Popen, PIPE, DEVNULL
 from urllib.request import urlretrieve
@@ -22,8 +23,10 @@ def _is_up(host: str, port: int) -> bool:
 class MinioServer:
 
     MINIO_URLS = {
-        'Linux-x86_64': 'https://dl.min.io/server/minio/release/linux-amd64/minio',
-        'Darwin-x86_64': 'https://dl.min.io/server/minio/release/darwin-amd64/minio'
+        'Linux-x86_64': 'https://github.com/pgsty/silo/releases/download/RELEASE.2026-09-03T13-18-01Z/silo_20260903131801.0.0_linux_amd64.tar.gz',
+        'Linux-aarch64': 'https://github.com/pgsty/silo/releases/download/RELEASE.2026-09-03T13-18-01Z/silo_20260903131801.0.0_linux_arm64.tar.gz',
+        'Darwin-x86_64': 'https://github.com/pgsty/silo/releases/download/RELEASE.2026-09-03T13-18-01Z/silo_20260903131801.0.0_darwin_amd64.tar.gz',
+        'Darwin-arm64': 'https://github.com/pgsty/silo/releases/download/RELEASE.2026-09-03T13-18-01Z/silo_20260903131801.0.0_darwin_arm64.tar.gz'
     }
 
     CACHE_ROOT = Path(os.environ.get('XDG_CACHE_HOME', os.path.join(os.path.expanduser('~'), '.cache')))
@@ -38,11 +41,14 @@ class MinioServer:
 
     def _get_minio(self):
         minio_dir = MinioServer.CACHE_DIR / 'minio'
-        minio_path = minio_dir / 'minio'
+        minio_path = minio_dir / 'silo'
         if not os.path.exists(minio_path):
             os.makedirs(minio_dir, exist_ok=True)
             minio_url = MinioServer.MINIO_URLS[f'{platform.system()}-{platform.machine()}']
-            urlretrieve(minio_url, minio_path)
+            minio_download = minio_dir / "silo.tar.gz"
+            urlretrieve(minio_url, minio_download)
+            with tarfile.open(minio_download, "r:gz") as tf:
+                tf.extract("silo", path=minio_dir)
         minio_path.chmod(0o755)
         return minio_path
 
